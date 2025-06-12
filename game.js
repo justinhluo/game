ae715d80e29f26d9b18250ab4dd23f9e9288ff88
justinhluo <33584797+justinhluo@users.noreply.github.com>
2025-06-11 21:38:51 -0700

    added ui

--- game.js
+++ game.js
@@ -5,12 +5,11 @@ function getComputerChoice() {
     else return "scissors";
 }
 
-function getHumanChoice() {
+
    const rock = document.querySelector("#rock");
    const paper = document.querySelector("#paper");
    const scissors = document.querySelector("#scissors");
    rock.addEventListener('click', () => {
-    
     playRound("rock",getComputerChoice());
    });
    paper.addEventListener('click', () => {
@@ -19,67 +18,60 @@ function getHumanChoice() {
    scissors.addEventListener('click', () => {
     playRound("scissors",getComputerChoice());
    });
-}
+
 
 let humanScore = 0;
 let ComputerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     const p = document.querySelector("#p");
-    const div = document.querySelector("div");
+    const div = document.querySelector(".body");
     const pscore = document.querySelector("#ps");
     const cscore = document.querySelector("#cs");
     if (humanChoice == computerChoice) {
         p.textContent = (`Tie! You both played ${humanChoice}`);
-        div.appendChild(p);
+        div.insertBefore(p ,cs);
     }
     else if(humanChoice == "rock") {
         if(computerChoice == "paper") {
             p.textContent = (`You lose! Paper beats rock.`);
-            div.appendChild(p);
-            ComputerScore++;
-            // cscore.textContent = (`computer score: ${ComputerScore}`);
-            // div.insertBefore(cscore, p);
-            
+            div.insertBefore(p, cs);
+            ComputerScore++;           
         } else{
             p.textContent = (`You win! rock beats scissors.`);
-            div.appendChild(p);
-            humanScore++;
-            // pscore.textContent = (`player score: ${humanScore}`);
-            // div.insertBefore(pscore, cscore);            
+            div.insertBefore(p, cs);
+            humanScore++;        
         }
     }else if(humanChoice == "paper") {
         if(computerChoice == "scissors") {
             p.textContent = (`You lose! Scissors beats paper.`);
-            div.appendChild(p);
+            div.insertBefore(p, cs);
             ComputerScore++;
         } else{
             p.textContent = (`You Win! Paper beats rock.`);
-            div.appendChild(p);
+            div.insertBefore(p, cs);
             humanScore++;            
         }        
     }else if(humanChoice == "scissors"){
         if(computerChoice == "rock") {
             p.textContent = (`You lose! Rock beats scissors.`);
-            div.appendChild(p);
+            div.insertBefore(p, cs);
             ComputerScore++;
         } else{
             p.textContent = (`You win! Scissors beats paper.`);
-            div.appendChild(p);
+            div.insertBefore(p, cs);
             humanScore++;
         }        
     }
     cscore.textContent = (`computer score: ${ComputerScore}`);
-    div.insertBefore(cscore, p);
+    div.appendChild(cscore);
     pscore.textContent = (`player score: ${humanScore}`);
-    div.insertBefore(pscore, cscore); 
+    div.insertBefore(pscore, p); 
     if(humanScore == 5) {
         setTimeout(() => {
             if(!alert('YOU WIN!')){window.location.reload();}
         }, 100);
-        
-        
-        
+               
     }
     if(ComputerScore == 5){
          setTimeout(() => {
@@ -89,7 +81,7 @@ function playRound(humanChoice, computerChoice) {
 
 }
 
-getHumanChoice();
+
 
 
 
